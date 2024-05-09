61728887b049d4d0a88e55b240891d15f855ff58
rajon38 <45762739+rajon38@users.noreply.github.com>
2024-05-09 19:26:30 +0600

    project completed 05-09-2024

--- index.js
+++ index.js
@@ -4,143 +4,152 @@ const collegeData = require("./data/bd_collegeName_data.json")
 const madrashaData = require("./data/bd_madrashaName_data.json")
 const govtUniData = require("./data/public_Uni_data.json")
 const engSchoolData = require("./data/english_medium_data.json")
-const govtDentData = require("./data/govt_dental_medical_college_data.json")
-const govtMediData = require("./data/govt_medical_college_data.json")
-const pvtDentData = require("./data/private_dental_medical_college.json")
-const pvtMediData = require("./data/private_medical_college_data.json")
 const pvtUniData = require('./data/private_Uni_data.json')
+const nuData = require('./data/nu_Uni_data.json')
 
 
-//All school (eng-ban)
-function getAllSchools() {
-  return [banSchoolData, engSchoolData, madrashaData];
-}
-
-function getAllCollege(){
-  return [collegeData]
-}
+//All institute
 function getAllInstitute(){
-   return [banSchoolData,engSchoolData, madrashaData, collegeData, govtUniData, govtDentData, govtMediData, pvtUniData, pvtMediData, pvtDentData]
+   return [banSchoolData,engSchoolData, madrashaData, collegeData, govtUniData, pvtUniData, nuData].flatMap(dataset => dataset.map(item => item))
 }
-
-function getAllInstituteName() {
-  return getAllInstitute().flatMap(dataset => dataset.map(item => item.name)).filter(name => name);
+function getAllInstituteNames() {
+  return getAllInstitute().map(data => data.name);
 }
-
  function getFilterData(key,type){
-   return  getAllInstitute().flatMap(institute=> institute.filter(item => item[key] === type));
+   return  getAllInstitute().filter(item => item[key] === type);
 
-   // return [institute].filter(institute => institute !== undefined)
  }
  function getAllInstituteByType(type) {
    return  getFilterData('institutionType', type);
-   // return data.map(item => item)
  }
 
  function getAllInstituteNameByType(type){
-
-   const data = getFilterData('institutionType', type);
-
-
-   return data.map(item => item.name);
+   return getFilterData('institutionType', type).map(item => item.name);
  }
 
+
+ //all school (bengali, english, arabic)
+function getAllSchools() {
+  return [banSchoolData, engSchoolData, madrashaData].flatMap(dataset => dataset.map(item => item));
+}
+
 function getAllSchoolNames() {
-  const allNames = getAllSchools().flatMap(school => school.map(item =>item.name));
-  return allNames.filter(name => name)
+  return  getAllSchools().map(school => school.name)
 
 }
-
 function getAllSchoolEiins() {
-  const allEiins = getAllSchools().flatMap(school => school.map(item => item.eiin))
-  return allEiins.filter(eiin => eiin)
+  return getAllSchools().map(school => school.eiin);
 }
-
 function getAllSchoolByVersion(version) {
-  const filterSchool = getAllSchools().flatMap(school => school.filter(item => item.version === version))
+  const filterSchool = getAllSchools().filter(item => item.version === version)
   return filterSchool.filter(school => school !== undefined)
 }
-
 function getAllNameSchoolByVersion(version) {
-  const filterSchool = getAllSchools().flatMap(school => school.filter(item => item.version === version).map((school)=> school.name))
+  const filterSchool = getAllSchools().filter(item => item.version === version).map((school)=> school.name)
   return filterSchool.filter(school => school !== undefined)
 }
-
 function getAllSchoolEiinByVersion(version) {
-  const filterSchool = getAllSchools().flatMap(school => school.filter(item => item.version === version).map((school)=> school.eiin))
+  const filterSchool = getAllSchools().filter(item => item.version === version).map((school)=> school.eiin)
   return filterSchool.filter(school => school !== undefined)
 }
-//
 function getSchoolByEiin(eiin) {
-  const filterSchool = getAllSchools().flatMap(school => school.filter(item => item.eiin === eiin));
+  const filterSchool = getAllSchools().filter(item => item.eiin === eiin);
   return filterSchool.filter(school => school !==undefined)
 }
-
 function getSchoolNameByEiin(eiin) {
-  const filterSchool = getAllSchools().flatMap(school => school.filter(item => item.eiin === eiin));
-  const sclData = filterSchool.map(school => school.name)
-  return sclData.filter(school => school !==undefined)
+  const filterSchool = getAllSchools().filter(item => item.eiin === eiin).map(school => school.name)
+  return filterSchool.filter(school => school !==undefined)
 }
-
-
-
 function getSchoolByName(name) {
-  const filterSchool = getAllSchools().flatMap(school => school.filter(item => item.name === name));
+  const filterSchool = getAllSchools().filter(item => item.name === name);
   return filterSchool.filter(school => school !==undefined)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getAllBanSclEiins() {
-//   return banSchoolData.map((school) => school.eiin);
-// }
-//
-// function getSchoolByEiin(eiin) {
-//   return banSchoolData.find((school) => school.eiin === eiin);
-// }
-//
-// function getSchoolNameByEiin(eiin) {
-//   const school = getSchoolByEiin(eiin);
-//   return school ? school.name : "School Not Found!";
-// }
-//
-// function getSchoolByName(name) {
-//   return banSchoolData.find(
-//     (school) => school.name.toLowerCase() === name.toLowerCase()
-//   );
-// }
-// function getNameByType(type) {
-//   return banSchoolData.filter(
-//     (school) => school.institutionType?.toLowerCase() === type.toLowerCase()
-//   );
-// }
 
 
 //college
-// function getAllCollege(){
-//   return collegeData;
-// }
-//
-// function getAllCollegeName(){
-//   return collegeData.map((college) => college.name);
-// }
+function getAllCollege(){
+  return [collegeData].flatMap(dataset => dataset.map(item => item))
+}
+function getAllCollegeNames() {
+  return getAllCollege().map(item => item.name)
+}
+
+function getAllCollegeEiins() {
+  return getAllCollege().map(item => item.eiin)
+}
+
+function getCollegeByEiin(eiin) {
+  const filterCollege = getAllCollege().filter(item => item.eiin === eiin);
+  return filterCollege.filter(college => college !==undefined)
+}
+
+function getCollegeNameByEiin(eiin) {
+  return getAllCollege().filter(item => item.eiin === eiin).map(college => college.name);
+}
+
+function getCollegeByName(name){
+  const filterCollege = getAllCollege().filter(item => item.name === name);
+  return filterCollege.filter(college => college !==undefined)
+}
+
+
+//university (govt, medical, pvt, nu)
+
+function getAllUnis() {
+  return [govtUniData, pvtUniData, nuData].flatMap(dataset => dataset.map(item => item))
+}
+
+function getAllUniNames() {
+  return getAllUnis().map(item => item.name)
+}
+
+function getUniByName(name){
+  const filterUnis = getAllUnis().filter(item => item.name === name);
+  return filterUnis.filter(unis => unis !==undefined)
+}
+
+function getUniByShortName(shortName) {
+  const filterUnis = getAllUnis().filter(item => item.shortName === shortName);
+  return filterUnis.filter(unis => unis !==undefined)
+}
+
+function getUniNameByShortName(shortName) {
+  const filterUnis = getAllUnis().filter(item => item.shortName === shortName).map(unis => unis.name);
+  return filterUnis.filter(unis => unis !==undefined)
+}
+
+function getAllUniByCategory(category) {
+  const filterUnis = getAllUnis().filter(item => item.category === category);
+  return filterUnis.filter(unis => unis !==undefined)
+}
+
+function getAllUniNameByCategory(category) {
+  const filterUnis = getAllUnis().filter(item => item.category === category).map(unis => unis.name);
+  return filterUnis.filter(unis => unis !==undefined)
+}
+
+function getAllUnisByDepartment(department){
+  const filterUnis = getAllUnis().filter(item => item.department === department);
+  return filterUnis.filter(unis => unis !==undefined)
+}
+
+function getAllUniNameByDepartment(department){
+  const filterUnis = getAllUnis().filter(item => item.department === department).map(unis => unis.name);
+  return filterUnis.filter(unis => unis !==undefined)
+}
+
 
 
 
 module.exports = {
+  getAllInstitute,
+  getAllInstituteNames,
+  getAllInstituteByType,
+  getAllInstituteNameByType,
+
+
   getAllSchools,
-  getAllCollege,
   getAllSchoolNames,
   getAllSchoolEiins,
   getSchoolByEiin,
@@ -150,20 +159,21 @@ module.exports = {
   getSchoolNameByEiin,
   getAllSchoolEiinByVersion,
 
-  getAllInstitute,
-  getAllInstituteName,
-  getAllInstituteByType,
-  getAllInstituteNameByType,
+  getAllCollege,
+  getAllCollegeNames,
+  getAllCollegeEiins,
+  getCollegeByEiin,
+  getCollegeNameByEiin,
+  getCollegeByName,
 
 
-
-  //getAllSchoolNames,
-  //getAllBdSclEiins
-  // getAllBdSclEiins,
-  // getBdSchoolByEiin,
-  // getSchoolNameByEiin,
-  // getSchoolByName,
-  // getNameByType,
-  // getAllCollege,
-  // getAllCollegeName
+  getAllUnis,
+  getAllUniNames,
+  getUniByName,
+  getUniByShortName,
+  getUniNameByShortName,
+  getAllUniByCategory,
+  getAllUniNameByCategory,
+  getAllUnisByDepartment,
+  getAllUniNameByDepartment
 };
